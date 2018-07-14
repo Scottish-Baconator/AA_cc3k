fa776fc0ca38d5a8ef795d11de9c29c02499ca15
Scottish-Baconator <alic.yognout@gmail.com>
2018-07-13 23:41:26 -0400

    Fixed a thing

---
+++ Debug/enemy.d
@@ -0,0 +1,16 @@
+enemy.o: ../enemy.cc ../enemy.h ../character.h ../obj.h ../coord.h \
+ ../floor.h ../chamber.h ../textdisplay.h
+
+../enemy.h:
+
+../character.h:
+
+../obj.h:
+
+../coord.h:
+
+../floor.h:
+
+../chamber.h:
+
+../textdisplay.h:
---
+++ Debug/game.d
@@ -0,0 +1,31 @@
+game.o: ../game.cc ../game.h ../floor.h ../obj.h ../coord.h ../chamber.h \
+ ../textdisplay.h ../potion.h ../item.h ../player.h ../character.h \
+ ../potions.h ../shade.h ../enemy.h ../gold.h
+
+../game.h:
+
+../floor.h:
+
+../obj.h:
+
+../coord.h:
+
+../chamber.h:
+
+../textdisplay.h:
+
+../potion.h:
+
+../item.h:
+
+../player.h:
+
+../character.h:
+
+../potions.h:
+
+../shade.h:
+
+../enemy.h:
+
+../gold.h:
